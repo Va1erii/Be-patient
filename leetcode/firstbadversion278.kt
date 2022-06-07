3a71cc7f673fea9f0e3a177697861754d609717d
Valerii Popov <valerii.popov@wealth-park.com>
2022-06-07 21:40:06 +0900

    Int to Longgit add .git add .

---
+++ leetcode/firstbadversion278.kt
@@ -0,0 +1,23 @@
+class FirstBadVersion {
+    fun firstBadVersion(n: Int): Int {
+        var start = 0L
+        var end = n.toLong()
+        while (start < end) {
+            val m = (start + end) / 2
+            if (isBadVersion(m.toInt())) {
+                end = m
+            } else {
+                start = m + 1
+            }
+        }
+        return start.toInt()
+    }
+
+    fun isBadVersion(version: Int): Boolean {
+        return version == 1702766719
+    }
+}
+
+fun main() {
+    println(FirstBadVersion().firstBadVersion(2126753390))
+}
